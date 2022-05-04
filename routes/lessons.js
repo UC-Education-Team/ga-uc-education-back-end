82196b63484cfbdcbaadef4497bbd7cb1e79343f
MarcusLind42 <96486666+MarcusLind42@users.noreply.github.com>
2022-05-03 20:38:15 -0400

    protecting lessons routes

--- routes/lessons.js
+++ routes/lessons.js
@@ -1,5 +1,6 @@
 import { Router } from 'express'
 import * as lessonsCtrl from '../controllers/lessons.js'
+import { checkAuth } from '../middleware/auth.js'
 
 const router = Router ()
 
@@ -7,6 +8,6 @@ const router = Router ()
 router.get('/', lessonsCtrl.index)
 router.get('/lessons/:id', lessonsCtrl.show)
 /*---------- Protected Routes ----------*/
-router.post('/lessons', lessonsCtrl.create)
-router.put('/lessons/:id', lessonsCtrl.update)
-router.delete('/lessons/:id', lessonsCtrl.delete)
+router.post('/lessons', checkAuth, lessonsCtrl.create)
+router.put('/lessons/:id', checkAuth, lessonsCtrl.update)
+router.delete('/lessons/:id', checkAuth, lessonsCtrl.delete)
