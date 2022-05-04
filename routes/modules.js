6e298b8899172f0e4d7679151a30b60d29dd1c0a
MarcusLind42 <96486666+MarcusLind42@users.noreply.github.com>
2022-05-04 15:39:48 -0400

    writing api call

--- routes/modules.js
+++ routes/modules.js
@@ -1,15 +1,16 @@
-import { Router } from 'express'
-import * as moduleCtrl from '../controllers/modules.js'
-import { checkAuth } from '../middleware/auth.js'
+import { Router } from "express";
+import * as moduleCtrl from "../controllers/modules.js";
+import { checkAuth } from "../middleware/auth.js";
 
-const router = Router()
+const router = Router();
 
 /*---------- Public Routes ----------*/
-router.get('/', moduleCtrl.index)
-router.get('/modules/:id', moduleCtrl.show)
+// router.get('/', moduleCtrl.index)
+router.get("/modules/:id", moduleCtrl.show);
 /*---------- Protected Routes ----------*/
-router.post('/modules', checkAuth, moduleCtrl.create)
-router.put('/modules/:id', checkAuth, moduleCtrl.update)
-router.delete('/modules/:id', checkAuth, moduleCtrl.delete)
+router.post("/modules", checkAuth, moduleCtrl.create);
+router.put("/modules/:id", checkAuth, moduleCtrl.update);
+router.delete("/modules/:id", checkAuth, moduleCtrl.delete);
+router.get("/", moduleCtrl.moduleCall);
 
-export { router }
+export { router };
